2f64dafde48e9679796535f2369160007a142ac3
Slimwolff <ptudoandtodos@gmail.com>
2024-11-04 17:53:51 -0300

    fix: now noise return array of algorithm

--- src/noise.js
+++ src/noise.js
@@ -1,8 +1,7 @@
 import fractal from 'fractal-noise';
 
-export const noise2 = (x, y) => {return x,y}
+export const noise2 = (x, y) => {return (x/3)/(y/3)}
 
 export default function Noise(x, y, options ) {
-    fractal.makeRectangle(250, 250, noise2, options)
+    return fractal.makeRectangle(250, 250, noise2, options)
 }
-

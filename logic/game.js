63034fdc0e4f2109d6865b69c3bfd34567738a37
Slimwolff <ptudoandtodos@gmail.com>
2024-11-07 17:31:35 -0300

    feat: add all classes to deal with game logic

---
+++ logic/game.js
@@ -0,0 +1,45 @@
+class Match {
+    constructor(gameID, players, map, ) {
+        this.id = gameID
+        this.players = players
+        this.map = map
+    }
+}
+
+class Map {
+    constructor(seed, mapGrid) {
+        this.seed = seed == undefined ? null : seed;
+        this.mapGrid = mapGrid;
+        this.tile = [];
+        this.units = [];
+        this.buildings = [];
+    }
+
+    buildMap() {
+        for(let x = 0; x < this.mapGrid; x++) {
+            this.tile[x] = []
+            for (let y = 0; y < this.mapGrid; y++) {
+                this.tile[x][y] = new MapTile();
+            }
+        }
+    }
+
+    moveUnit() {
+
+    }
+    newBuilding() {
+
+    }
+    demolishBuilding() {
+
+    }
+}
+
+class MapTile {
+    constructor() {
+        this.terrain = "";
+        this.unit = {};
+        this.building = {};
+        this.owner = ""
+    }
+}

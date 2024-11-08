ba78144c49a1ec1a3072c22f5493ad6c089ccefa
Slimwolff <ptudoandtodos@gmail.com>
2024-11-08 17:28:45 -0300

    feat: add character logic

---
+++ logic/character.js
@@ -0,0 +1,49 @@
+class Character {
+    constructor(stats) {
+        this.type = stats.type;
+        this.health = stats.health;
+        this.atk = stats.atk;
+        this.range = stats.range
+        this.movTile = stats.movtile
+        this.x = 0;
+        this.y = 0;
+    }
+
+    move(x,y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    atk() {
+
+    }
+
+    receiveAtk(enemyAtk) {
+        //decrease health of character
+        this.health -= enemyAtk
+
+        // if health reach 0 or below units dies -> destroy unit method
+        if(this.health <= 0) {
+            this.destroyUnit()
+        }
+    }
+
+    destroyUnit() {
+
+    }
+
+}
+
+
+let ARCHER = {
+    type: 'archer',
+    health: 10,
+    atk: 3,
+    range: 5,
+    movtile: 3
+}
+
+export default {
+    ARCHER,
+    Character
+} 

ff9d7c857979baa6c60eacf8fac2ab8ed913332a
Slimwolff <ptudoandtodos@gmail.com>
2024-11-13 17:19:31 -0300

    fix: now module export every class as itself

--- models/index.js
+++ models/index.js
@@ -1,7 +1,9 @@
-// import User from './user.js'
-// import Player from './player.js'
-// export default {
-//  Player,
-//  User
-// }
+import User from './user.js'
+import Player from './player.js'
+import Room from './game.js'
+export {
+    Player,
+    User,
+    Room
+}
 //This is a module to export all models

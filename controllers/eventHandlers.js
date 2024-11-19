fa72b4e0716b13c0081028b7052054143be86ad2
Slimwolff <ptudoandtodos@gmail.com>
2024-11-19 17:20:22 -0300

    feat: add constant USER_SESSIONS

--- controllers/eventHandlers.js
+++ controllers/eventHandlers.js
@@ -1,24 +1,24 @@
 import {Room, User} from "../models/index.js"
 
-const ROOMS = {
-    $: []
-}
+const ROOMS = []
+
+const USER_SESSION = [];
 
 // Create game room to host the match
 function gameRoom(socket) {
     
     // Create new room to host
-    socket.on('newHost', (a,b)=> {
+    socket.on('newHost', (a,)=> {
         console.log('newHost requested!!',a,b)
     })
     // Client search for active host server anwser list of rooms availabe
     socket.on('fetchHosts', () => {
-        socket.emit('hostsAvailable', ROOMS.$)
+        socket.emit('hostsAvailable', ROOMS)
     })
 
     // Client joins to target host if possible
     socket.on('joinRoom', (roomIndex,userSession) => {
-        ROOMS.$[roomIndex].addUser(userId)
+        ROOMS[roomIndex].addUser(userId)
     })
 
 }
